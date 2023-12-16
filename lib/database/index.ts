ebc7f0be71cd18a288499b7774880eed6e7c38af
exslym <76844562+exslym@users.noreply.github.com>
2023-12-16 22:18:45 +0400

    mongoose db renamed

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -12,7 +12,7 @@ export const connectToDatabase = async () => {
 	cached.promise =
 		cached.promise ||
 		mongoose.connect(MONGODB_URI, {
-			dbName: 'Eventlify',
+			dbName: 'eventlify',
 			bufferCommands: false,
 		});
 
