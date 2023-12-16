46cf3e8497914e0d0e11e4632fbebddc3d33a221
exslym <76844562+exslym@users.noreply.github.com>
2023-12-16 16:30:31 +0400

    footer updated

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className='border-t'>
+			<div className='wrapper flex flex-col items-start justify-start'>
+				<Link href='/'>
+					<div className='flex items-center justify-start gap-3'>
+						<Image src='/assets/images/logo.png' alt='logo' width={24} height={24} />
+						<h2 className='text-xl font-semibold'>Eventlify</h2>
+					</div>
+				</Link>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
