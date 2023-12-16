13d8ca6745ac4c8017827379b601a32bb8d3a2ab
exslym <76844562+exslym@users.noreply.github.com>
2023-12-17 02:20:29 +0400

    timeOptions changed to 24h

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,7 +16,7 @@ export const formatDateTime = (dateString: Date) => {
 		day: 'numeric', // numeric day of the month (e.g., '25')
 		hour: 'numeric', // numeric hour (e.g., '8')
 		minute: 'numeric', // numeric minute (e.g., '30')
-		hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+		hour12: false, // use 12-hour clock (true) or 24-hour clock (false)
 	};
 
 	const dateOptions: Intl.DateTimeFormatOptions = {
@@ -29,7 +29,7 @@ export const formatDateTime = (dateString: Date) => {
 	const timeOptions: Intl.DateTimeFormatOptions = {
 		hour: 'numeric', // numeric hour (e.g., '8')
 		minute: 'numeric', // numeric minute (e.g., '30')
-		hour12: true, // use 12-hour clock (true) or 24-hour clock (false)
+		hour12: false, // use 12-hour clock (true) or 24-hour clock (false)
 	};
 
 	const formattedDateTime: string = new Date(dateString).toLocaleString('en-US', dateTimeOptions);
