d8063737e578d426eb1f92abaabd6920a00cb718
exslym <76844562+exslym@users.noreply.github.com>
2023-12-17 12:58:00 +0400

    user.actions updated

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,11 +2,11 @@
 
 import { revalidatePath } from 'next/cache';
 
-import { connectToDatabase } from '@/lib/database';
-import Event from '@/lib/database/models/event.model';
-import Order from '@/lib/database/models/order.model';
-import User from '@/lib/database/models/user.model';
-import { handleError } from '@/lib/utils';
+import { connectToDatabase } from '../database';
+import Event from '../database/models/event.model';
+import Order from '../database/models/order.model';
+import User from '../database/models/user.model';
+import { handleError } from '../utils';
 
 import { CreateUserParams, UpdateUserParams } from '@/types';
 
