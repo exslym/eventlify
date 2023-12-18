0b1487609b29cbd06184ea64a5c7cfb263629e3d
exslym <76844562+exslym@users.noreply.github.com>
2023-12-18 14:18:52 +0400

    Footer updated, user.model updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css';
 
 const poppins = Poppins({
 	subsets: ['latin'],
-	weight: ['400', '500', '600', '700'],
+	weight: ['300', '400', '500', '600', '700'],
 	variable: '--font-poppins',
 });
 
