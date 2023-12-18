a761078a15ecb6dc7a93677b4b8c2bc2cb688a9c
exslym <76844562+exslym@users.noreply.github.com>
2023-12-18 23:39:07 +0400

    preview updated

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// images: {
-	// 	domains: ['utfs.io'],
-	// 	remotePatterns: [
-	// 		{
-	// 			protocol: 'https',
-	// 			hostname: 'utfs.io',
-	// 			port: '',
-	// 		},
-	// 	],
-	// },
 	images: {
 		remotePatterns: [
 			{
 				protocol: 'https',
 				hostname: 'utfs.io',
-				// hostname: 'res.cloudinary.com',
 				pathname: '**',
 			},
 		],
