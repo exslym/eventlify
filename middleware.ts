6df6728a1f4ab4d3409f3342f084c2fd0d44d52c
exslym <76844562+exslym@users.noreply.github.com>
2023-12-18 15:42:26 +0400

    updated

--- middleware.ts
+++ middleware.ts
@@ -8,13 +8,17 @@ export default authMiddleware({
 		'/api/webhook/stripe',
 		'/api/uploadthing',
 	],
-	// ignoredRoutes: ['/api/webhook/clerk', '/api/webhook/stripe', '/api/uploadthing'],
-	debug: true,
+	ignoredRoutes: ['/api/webhook/clerk', '/api/webhook/stripe', '/api/uploadthing'],
+	// debug: true,
 });
 
 export const config = {
 	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// export const config = {
+// 	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
 
 // export const config = {
 // 	matcher: ['/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)', '/'],
